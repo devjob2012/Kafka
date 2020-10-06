30b068e23ce9cdb394d62cab96908309c4f2d8e7
deven <devendra.rawat@ext.ons.gov.uk>
2020-10-06 18:33:25 +0100

    Added classes

--- src/main/java/co/uk/deven/kafka/kafkaproject/KafkaProjectApplication.java
+++ src/main/java/co/uk/deven/kafka/kafkaproject/KafkaProjectApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkaProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProjectApplication.class, args);
+        System.out.println("Hello");
+    }
 
 }
--- src/main/java/co/uk/deven/kafka/kafkaproject/producer/ProducerDemo.java
+++ src/main/java/co/uk/deven/kafka/kafkaproject/producer/ProducerDemo.java
@@ -1,13 +1,11 @@
 package co.uk.deven.kafka.kafkaproject.producer;
 
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 
 import java.util.Arrays;
 
 public class ProducerDemo {
     public void produce() {
-        KafkaProperties.Producer prod = new KafkaProperties.Producer();
-        prod.setBootstrapServers(Arrays.asList("localhost:9092"));
+
 
     }
 }
